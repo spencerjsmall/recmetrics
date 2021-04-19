afcc8c2883e9a696526426f1cc91d7c16cd1d7a7
Spencer Small <61337412+spencerjsmall@users.noreply.github.com>
2021-04-19 10:35:02 -0700

    remote precision recall from init

--- recmetrics/__init__.py
+++ recmetrics/__init__.py
@@ -3,4 +3,4 @@ from .metrics import (catalog_coverage, intra_list_similarity,
                       personalization, prediction_coverage,
                       recommender_precision, recommender_recall, rmse)
 from .plots import (class_separation_plot, coverage_plot, long_tail_plot,
-                    mapk_plot, mark_plot,  metrics_plot, precision_recall_plot, roc_plot)
+                    mapk_plot, mark_plot,  metrics_plot, roc_plot)
